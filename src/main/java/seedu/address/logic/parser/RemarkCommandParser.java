b25142cc4c43f43beb986bdb250adf3586294419
Wincent <wincenttsjoi@gmail.com>
2020-09-15 02:50:23 +0800

    Complete tutorial for adding command:

--- src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
+++ src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
@@ -15,6 +15,14 @@ import seedu.address.model.person.Remark;
  */
 public class RemarkCommandParser implements Parser<RemarkCommand> {
 
+    /**
+     * Parses the given {@code String} of arguments in the context of the AddCommand
+     * and returns an AddCommand object for execution.
+     *
+     * @param args
+     * @return RemarkCommand
+     * @throws ParseException if the user input does not conform the expected format
+     */
     public RemarkCommand parse(String args) throws ParseException {
         requireNonNull(args);
         ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args,
